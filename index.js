596c43e982c70f373466601929ecfed0beb4f064
Christerle_Nounoute <christerlesn@gmail.com>
2021-08-08 21:35:48 -0700

    commit tree 7cec652e1e12787d228a8b0c92b4ec8f442f1cb5

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(main = '*'){
+    return function(important = "special"){
+        return `You are ${main}${important}${main}!`
+    }
+}
+
+const Calculator = {
+    add: function add(n1, n2){
+        const results = n1 + n2
+        return results
+    },
+
+    subtract: function subtract(n1, n2){
+        const results = n1 - n2
+        return results
+    },
+
+    multiply: function multiply(n1, n2){
+        const results = n1 * n2
+        return results
+    },
+
+    divide: function divide(n1, n2){
+        const results = n1 / n2
+        return results
+    }
+}
+
+function actionApplyer(int, array){
+    array.forEach(fn => int = fn(int))
+    return int
+}
